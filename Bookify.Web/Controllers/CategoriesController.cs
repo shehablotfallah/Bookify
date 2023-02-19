fb33a468008faa2fcdbb9f175d016d8c8613a302
Shehab Lotfallah <shehabw126@gmail.com>
2023-02-19 19:12:57 +0200

    Add Buttons action to categories Add Fonts and validation

--- Bookify.Web/Controllers/CategoriesController.cs
+++ Bookify.Web/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bookify.Web.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Web.Controllers
 {
@@ -11,10 +13,78 @@ namespace Bookify.Web.Controllers
             _context = context;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
-            var categories = _context.Categories.ToList();
+            var categories = _context.Categories.AsNoTracking().ToList();
             return View(categories);
         }
+
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View("Form");
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Create(CategoryFormViewModel model)
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View("Form",model);
+            }
+            else
+            {
+                var category = new Category { Name= model.Name };
+                _context.Add(category);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var viewmodel = new CategoryFormViewModel
+                {
+                    Id = id,
+                    Name = category.Name 
+                };
+                return View("Form", viewmodel);
+            }
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Edit(CategoryFormViewModel model)
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View("Form", model);
+            }
+            else
+            {
+                var category = _context.Categories.Find(model.Id);
+                if (category is null) 
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    category.Name = model.Name;
+                    category.LastUpdatedOn = DateTime.Now;
+                    _context.SaveChanges();
+
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+        }  
     }
 }
---
+++ Bookify.Web/Core/ViewModels/CategoryFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookify.Web.Core.ViewModels
+{
+    public class CategoryFormViewModel
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Max length cannot be more than 100 letters!")]
+        public string Name { get; set; } = null!;
+
+    }
+}
